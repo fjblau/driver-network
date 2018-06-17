aa378cfe6b500f51d8bc949225c84b5319128fd7
fjblau <fjblau@gmail.com>
2018-06-17 11:12:55 +0200

    Commit version 0.0.28

--- start.sh
+++ start.sh
@@ -16,6 +16,10 @@ archiveFile="driver-network@$1.bna"
 bash ~/fabric-dev-servers/startFabric.sh
 
 rm *.bna
+git rm *.bna
+git commit -m "Commit version $1"
+git push -u origin master
+
 composer archive create -t dir -n .
 composer network install -c PeerAdmin@hlfv1 -a $archiveFile
 composer network start --card PeerAdmin@hlfv1 --networkAdmin admin  --networkName driver-network --networkVersion "$1" --networkAdminEnrollSecret adminpw  --file networkadmin.card
