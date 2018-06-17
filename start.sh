03d363408587e828c9e11694de1d9ad7e1e34527
fjblau <fjblau@gmail.com>
2018-06-17 11:03:35 +0200

    Commit version 0.0.26

--- start.sh
+++ start.sh
@@ -20,7 +20,7 @@ composer archive create -t dir -n .
 composer network install -c PeerAdmin@hlfv1 -a $archiveFile
 composer network start --card PeerAdmin@hlfv1 --networkAdmin admin  --networkName driver-network --networkVersion "$1" --networkAdminEnrollSecret adminpw  --file networkadmin.card
 
-
+git rm *.bna
 git add *
 git commit -m "Commit version $1"
 git push -u origin master
